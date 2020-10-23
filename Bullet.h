5b260720e683827465a4c8cf071540fa73bdc57f
500071061 <55651789+500071061@users.noreply.github.com>
2020-10-23 13:07:35 +0530

    Add files via upload

---
+++ Bullet.h
@@ -0,0 +1,192 @@
+bool GameActive = true;
+//Bullet for Tank
+static const int Max_Bullet = 5;
+static const double Bullet_Speed = 5.0;
+
+struct BulletInfo
+{
+    bool IsActive = false;
+    int X_Pos=0, Y_Pos=0;
+    double angle=0.0;
+    clock_t ReleaseTime = 0;
+};
+
+BulletInfo Bullet1[Max_Bullet];
+BulletInfo Bullet2[Max_Bullet];
+clock_t LastBulletFired = clock();
+
+
+
+void NewBullet1()
+{
+    clock_t CurrentTime = clock();
+    int TimeInterval = int ((CurrentTime - LastBulletFired)/CLOCKS_PER_SEC);
+    if(TimeInterval<1)
+        return;
+
+    int i;
+
+    for(i=0;i<Max_Bullet;i++)
+    {
+        if(Bullet1[i].IsActive==false)
+        {
+            break;
+        }
+    }
+
+    if(i==Max_Bullet)
+        return;
+
+    Bullet1[i].IsActive = true;
+    Bullet1[i].angle = angle1 - M_PI_4;
+    Bullet1[i].X_Pos = Tank1X + (int)(40.0*cos(angle1-M_PI_4));
+    Bullet1[i].Y_Pos = Tank1Y - (int)(40.0*sin(angle1-M_PI_4));
+    Bullet1[i].ReleaseTime = CurrentTime;
+    LastBulletFired = CurrentTime;
+}
+
+void NewBullet2()
+{
+    clock_t CurrentTime = clock();
+    int TimeInterval = int ((CurrentTime - LastBulletFired)/CLOCKS_PER_SEC);
+    if(TimeInterval<1)
+        return;
+
+    int i;
+
+    for(i=0;i<Max_Bullet;i++)
+    {
+        if(Bullet2[i].IsActive==false)
+        {
+            break;
+        }
+    }
+
+    if(i==Max_Bullet)
+        return;
+
+    Bullet2[i].IsActive = true;
+    Bullet2[i].angle = angle2 - M_PI_4;
+    Bullet2[i].X_Pos = Tank2x + (int)(40.0*cos(angle2-M_PI_4));
+    Bullet2[i].Y_Pos = Tank2y - (int)(40.0*sin(angle2-M_PI_4));
+    Bullet2[i].ReleaseTime = CurrentTime;
+    LastBulletFired = CurrentTime;
+}
+
+void MoveBullet1()
+{
+    clock_t CurrentTime = clock();
+    int BulletSurvivalTime;
+
+    for(int i=0; i<Max_Bullet; i++)
+        {
+            if(Bullet1[i].IsActive)
+            {
+                BulletSurvivalTime = int ((CurrentTime - Bullet1[i].ReleaseTime)/CLOCKS_PER_SEC);
+                if(BulletSurvivalTime > 10)
+                {
+                    Bullet1[i].IsActive = false;
+                    Bullet1[i].angle = 0.0;
+                    Bullet1[i].X_Pos = 0;
+                    Bullet1[i].Y_Pos = 0;
+                    Bullet1[i].ReleaseTime = 0;
+                    continue;
+                }
+
+
+                    Bullet1[i].X_Pos += (int)(Bullet_Speed * cos(Bullet1[i].angle));
+                    Bullet1[i].Y_Pos -= (int)(Bullet_Speed * sin(Bullet1[i].angle));
+
+                    if(getpixel(Bullet1[i].X_Pos, Bullet1[i].Y_Pos) == BLACK &&
+                   (getpixel(Bullet1[i].X_Pos+2, Bullet1[i].Y_Pos+2) == BLACK || getpixel(Bullet1[i].X_Pos-2, Bullet1[i].Y_Pos+2) == BLACK ||
+                    getpixel(Bullet1[i].X_Pos+2, Bullet1[i].Y_Pos-2) == BLACK || getpixel(Bullet1[i].X_Pos-2, Bullet1[i].Y_Pos-2) == BLACK))
+                    {
+                        Bullet1[i].X_Pos -= (int)(Bullet_Speed * cos(Bullet1[i].angle));
+
+                        if(getpixel(Bullet1[i].X_Pos, Bullet1[i].Y_Pos) == 0)
+                        {
+                            // DEFLECTING FROM HORIZONTAL SURFACE
+                            Bullet1[i].Y_Pos += (int)(Bullet_Speed * sin(Bullet1[i].angle));
+                            Bullet1[i].angle = -Bullet1[i].angle;
+                        }
+                        else
+                        {
+                            // DEFLEFCTING FROM VERTICAL SURFACE
+                            Bullet1[i].Y_Pos += (int)(Bullet_Speed * sin(Bullet1[i].angle));
+                            Bullet1[i].angle = M_PI - Bullet1[i].angle;
+                        }
+
+                    }
+                 if(getpixel(Bullet1[i].X_Pos, Bullet1[i].Y_Pos) == GREEN &&
+                   (getpixel(Bullet1[i].X_Pos+2, Bullet1[i].Y_Pos+2) == GREEN || getpixel(Bullet1[i].X_Pos-2, Bullet1[i].Y_Pos+2) == GREEN ||
+                    getpixel(Bullet1[i].X_Pos+2, Bullet1[i].Y_Pos-2) == GREEN || getpixel(Bullet1[i].X_Pos-2, Bullet1[i].Y_Pos-2) == GREEN))
+                {
+                    GameActive = false;
+                }
+
+                    circle(Bullet1[i].X_Pos, Bullet1[i].Y_Pos,2);
+
+            }
+        }
+
+}
+
+
+void MoveBullet2()
+{
+    clock_t CurrentTime = clock();
+    int BulletSurvivalTime;
+
+    for(int i=0; i<Max_Bullet; i++)
+        {
+            if(Bullet2[i].IsActive)
+            {
+                BulletSurvivalTime = int ((CurrentTime - Bullet2[i].ReleaseTime)/CLOCKS_PER_SEC);
+                if(BulletSurvivalTime > 10)
+                {
+                    Bullet2[i].IsActive = false;
+                    Bullet2[i].angle = 0.0;
+                    Bullet2[i].X_Pos = 0;
+                    Bullet2[i].Y_Pos = 0;
+                    Bullet2[i].ReleaseTime = 0;
+                    continue;
+                }
+
+
+                    Bullet2[i].X_Pos += (int)(Bullet_Speed * cos(Bullet2[i].angle));
+                    Bullet2[i].Y_Pos -= (int)(Bullet_Speed * sin(Bullet2[i].angle));
+
+                  if(getpixel(Bullet2[i].X_Pos, Bullet2[i].Y_Pos) == BLACK &&
+                   (getpixel(Bullet2[i].X_Pos+2, Bullet2[i].Y_Pos+2) == BLACK || getpixel(Bullet2[i].X_Pos-2, Bullet2[i].Y_Pos+2) == BLACK ||
+                    getpixel(Bullet2[i].X_Pos+2, Bullet2[i].Y_Pos-2) == BLACK || getpixel(Bullet2[i].X_Pos-2, Bullet2[i].Y_Pos-2) == BLACK))
+                    {
+                        Bullet2[i].X_Pos -= (int)(Bullet_Speed * cos(Bullet2[i].angle));
+
+                        if(getpixel(Bullet2[i].X_Pos, Bullet2[i].Y_Pos) == 0)
+                        {
+                            // DEFLECTING FROM HORIZONTAL SURFACE
+                            Bullet2[i].Y_Pos += (int)(Bullet_Speed * sin(Bullet2[i].angle));
+                            Bullet2[i].angle = -Bullet2[i].angle;
+                        }
+                        else
+                        {
+                            // DEFLEFCTING FROM VERTICAL SURFACE
+                            Bullet2[i].Y_Pos += (int)(Bullet_Speed * sin(Bullet2[i].angle));
+                            Bullet2[i].angle = M_PI - Bullet2[i].angle;
+                        }
+
+                    }
+                    else if(getpixel(Bullet2[i].X_Pos, Bullet2[i].Y_Pos) == RED &&
+                   (getpixel(Bullet2[i].X_Pos+2, Bullet2[i].Y_Pos+2) == RED || getpixel(Bullet2[i].X_Pos-2, Bullet2[i].Y_Pos+2) == RED ||
+                    getpixel(Bullet2[i].X_Pos+2, Bullet2[i].Y_Pos-2) == RED || getpixel(Bullet2[i].X_Pos-2, Bullet2[i].Y_Pos-2) == RED))
+                    {
+                        GameActive = false;
+
+                    }
+
+                    circle(Bullet2[i].X_Pos, Bullet2[i].Y_Pos,2);
+
+            }
+        }
+
+}
---
+++ minor.cpp
@@ -0,0 +1,218 @@
+#include<graphics.h>
+#include<windows.h>
+#include<math.h>
+#include<conio.h>
+#include<time.h>
+#include"map.h"
+#include"Tank.h"
+#include"Bullet.h"
+
+void Menu()
+{
+    //Home Screen window
+    initwindow(900,600,"HOME",100,100);
+    POINT CursorPosition;
+
+    while(GameActive)
+    {
+        settextstyle(9,HORIZ_DIR,5);
+        setcolor(RED);
+        outtextxy(200,100,"TANK GAME");
+        settextstyle(9,HORIZ_DIR,1);
+        setcolor(WHITE);
+        outtextxy(300,200,"MENU");
+        outtextxy(220,250,"1. START");
+        outtextxy(220,300,"2. HOW TO PLAY");
+
+
+        if(GetAsyncKeyState(VK_LBUTTON))
+        {
+            GetCursorPos(&CursorPosition);
+            //Mouse Click To start the game
+            if((CursorPosition.x>=350 && CursorPosition.x<=430) && (CursorPosition.y>=370 && CursorPosition.y<=390))
+            {
+                //loading screen
+                delay(500);
+                outtextxy(220,450,"LOADING");
+                delay(500);
+                outtextxy(340,450,".");
+                delay(500);
+                outtextxy(360,450,".");
+                delay(500);
+                outtextxy(380,450,".");
+                delay(500);
+                outtextxy(400,450,".");
+                break;
+            }
+            //Mouse click for Instructions
+            if((CursorPosition.x>=350 && CursorPosition.x<=510) && (CursorPosition.y>=420 && CursorPosition.y<=440))
+            {
+                //INSTRUCTIONS Window
+                initwindow(900,600,"INSTRUCTIONS",100,100,false,false);
+                while(1)
+                {
+                    outtextxy(50,50,"PLAYER 1:");
+                    outtextxy(750,470,"START >>");
+
+                    if(GetAsyncKeyState(VK_LBUTTON))
+
+                    {   GetCursorPos(&CursorPosition);
+                        //mouse click to start the game from instructions window
+                        if((CursorPosition.x>=850 && CursorPosition.x<=920) && (CursorPosition.y>=590 && CursorPosition.y<=610))
+                        {
+                            settextstyle(9,HORIZ_DIR,1);
+                            delay(500);
+                            outtextxy(220,550,"LOADING");
+                            delay(500);
+                            outtextxy(340,550,".");
+                            delay(500);
+                            outtextxy(360,550,".");
+                            delay(500);
+                            outtextxy(380,550,".");
+                            delay(500);
+                            outtextxy(400,550,".");
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+
+         }
+
+    }
+}
+
+int main()
+{
+    label:
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
+
+  // Menu();
+
+    initwindow(width, height, "Tank Trouble", -3, 0, false, true);
+    int page=0,i=0,j=0;
+
+    while(GameActive)
+    {
+        setactivepage(page);
+        setvisualpage(1-page);
+        cleardevice();
+        T1Coordinates();
+        T2Coordinates();
+        DrawMap();
+
+        if(GetAsyncKeyState(VK_UP))
+        {
+            MoveFront();
+            T1Coordinates();
+            if(ConditionCheck()==0)
+            {
+                MoveBack();
+                T1Coordinates();
+            }
+        }
+        if(GetAsyncKeyState(VK_DOWN))
+        {
+            MoveBack();
+            T1Coordinates();
+            if(ConditionCheck()==0)
+            {
+                MoveFront();
+                T1Coordinates();
+            }
+        }
+       if(GetAsyncKeyState(VK_RIGHT))
+        {
+            angle1 -= M_PI/45.0;
+            degree1 -=4;                   //4 degree
+            T1Coordinates();
+             if(ConditionCheck()==0)
+            {
+                angle1 += M_PI/45.0;
+                degree1 +=4;
+                T1Coordinates();
+            }
+        }
+        if(GetAsyncKeyState(VK_LEFT))
+        {
+            angle1 += M_PI/45.0;
+            degree1 +=4;                  //4 degree
+            T1Coordinates();
+            if(ConditionCheck()==0)
+            {
+                angle1 -= M_PI/45.0;
+                degree1 -=4;
+                T1Coordinates();
+            }
+        }
+        if(GetAsyncKeyState(VK_RCONTROL))
+        {
+            NewBullet1();
+        }
+         if(GetAsyncKeyState('W'))
+        {
+            MoveFront2();
+            T2Coordinates();
+            if(ConditionCheck2()==0)
+            {
+                MoveBack2();
+                T2Coordinates();
+            }
+        }
+        if(GetAsyncKeyState('S'))
+        {
+            MoveBack2();
+            T2Coordinates();
+            if(ConditionCheck2()==0)
+            {
+                MoveFront2();
+                T2Coordinates();
+            }
+        }
+       if(GetAsyncKeyState('D'))
+        {
+            angle2 -= M_PI/45.0;
+            degree2 -=4;                   //4 degree
+            T2Coordinates();
+             if(ConditionCheck2()==0)
+            {
+                angle2 += M_PI/45.0;
+                degree2 +=4;
+                T2Coordinates();
+            }
+        }
+        if(GetAsyncKeyState('A'))
+        {
+            angle2 += M_PI/45.0;
+            degree2 +=4;                  //4 degree
+            T2Coordinates();
+            if(ConditionCheck2()==0)
+            {
+                angle2 -= M_PI/45.0;
+                degree2 -=4;
+                T2Coordinates();
+            }
+        }
+        if(GetAsyncKeyState(VK_LCONTROL))
+        {
+            NewBullet2();
+        }
+
+        if(GetAsyncKeyState(VK_ESCAPE))
+        {
+            break;
+        }
+        MoveBullet1();
+        MoveBullet2();
+        DrawTank();
+        DrawTank2();
+        page=1-page;
+        delay(10);
+     }
+
+    getch();
+    closegraph();
+    goto label;
+}
